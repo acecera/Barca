1a43579d273fbc631a5cfa302b73728fa91d9bc7
Anthony Cera <ace_cera@yahoo.com>
2019-06-06 18:06:27 -0700

    added an app element browserRoute and routes tags.

--- fcbarcelona/src/index.js
+++ fcbarcelona/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Routes from './routes';
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
