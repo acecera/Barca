fae2a638307754dc7a2b5b851c3ea3acba69eeaa
Anthony Cera <ace_cera@yahoo.com>
2019-06-09 11:30:36 -0700

    imported Footer component and added it to the layout.

--- fcbarcelona/src/hoc/Layout/layout.js
+++ fcbarcelona/src/hoc/Layout/layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './layout.css';
 import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
 
 
 class Layout extends Component {
@@ -14,6 +15,7 @@ class Layout extends Component {
             <div>
                 <Header/>
                 {this.props.children}
+                <Footer/>
             </div>
         )
     }
